9de7ae6ccbd9ab9664968346485931c0e5acd94f
Husniddin Tugizov <husniddindev@gmail.com>
2024-05-16 00:11:31 +0500

    Mohirdev task

---
+++ task-5/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class SumOfNumbers
+{
+    static void Main()
+    {
+        Console.WriteLine("N musbat butun sonni kiriting:");
+        int N = Convert.ToInt32(Console.ReadLine());
+
+        int sum = 0;
+        for (int i = 1; i <= N; i++)
+        {
+            sum += i;
+        }
+
+        Console.WriteLine($"1 dan {N} gacha bo'lgan sonlar yig'indisi: {sum}");
+    }
+}
